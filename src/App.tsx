c107a4335864ad16fe4ff8508487ecb100d60f8c
Owen Wang <o0wang@outlook.com>
2025-09-10 00:25:59 -0400

    Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {ChevronRight} from "lucide-react";
 function App() {
   return (
       <Template>
-          <div className="p-4 shadow-lg shadow-zinc-950 bg-zinc-800 rounded-xl border-0">
+          <div className="p-4 shadow-lg shadow-zinc-950 bg-zinc-800 rounded-xl border-0 mb-8">
               <TrafficLight />
               <h1 className="text-3xl font-bold mb-4">Woods CS is back for 2025!</h1>
               <p>
@@ -14,7 +14,23 @@ function App() {
               <p className="flex my-8 text-zinc-400 align-baseline">
                   Join us Monday lunches @ Room 100!
               </p>
-              <a href="#" className="bg-brand py-3 px-4 rounded-full text-white font-semibold inline-flex">See What's Happening <ChevronRight /></a>
+              <a href="#" className="btn primary">See What's Happening <ChevronRight /></a>
+              <hr className="my-8"/>
+              <h1 className="text-3xl font-bold mb-4">Gallery</h1>
+              <hr className="my-8"/>
+              <h1 className="text-3xl font-bold mb-4">Calendar</h1>
+              <hr className="my-8"/>
+              <h1 className="text-3xl font-bold mb-4">Gallery</h1>
+              <hr className="my-8"/>
+              <h1 className="text-3xl font-bold mb-4">Meeting Notes</h1>
+              <p>
+                  Past meeting notes and lesson material can be found on our website.
+              </p>
+              <a href="#" className="btn border-2 mt-8">Meeting Notes <ChevronRight /></a>
+              <hr className="my-8"/>
+              <h1 className="text-3xl font-bold mb-4">Hack::Peel</h1>
+
+
           </div>
       </Template>
   )
--- src/Template.tsx
+++ src/Template.tsx
@@ -1,33 +1,46 @@
-import {ChevronRight} from "lucide-react";
+import {ChevronDown, ChevronRight} from "lucide-react";
+import * as React from "react";
 
 export default function Template({children = []} : {children?: React.ReactNode}) {
-    return <main className="flex h-full w-300 max-w-full mx-auto p-8 items-start gap-8 flex-col md:flex-row">
-        <nav className="p-4 shadow-lg shadow-zinc-950 bg-zinc-800 rounded-xl border-0 flex-none text-left sticky top-8 md:w-auto w-full">
-            <a href="/" className="flex mb-5">
-                <img src="/logo.png" alt="logo" className="flex-none h-16 mr-4" />
-                <h1 className="text-2xl font-bold flex-auto">
-                    Woodlands <br/> Computer Science
-                </h1>
-            </a>
-            <div className="ml-18 flex flex-col gap-3">
-                <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
-                    <ChevronRight/>Home
+    const [ navOpen, setNavOpen ] = React.useState(false);
+    return <main className="p-4 flex h-full w-500 max-w-full mx-auto items-start gap-4 flex-col lg:flex-row">
+        <nav className={"p-4 transition-all duration-200 shadow-2xl shadow-black " +
+            "bg-zinc-900 border-1 border-t-0 lg:border-t-1 border-zinc-700 " +
+            "rounded-b-xl lg:rounded-xl " +
+            "flex-none text-left fixed lg:sticky top-0 right-4 left-4 lg:top-4 w-auto"}
+        >
+            <div className="flex justify-between">
+                <a href="/" className="flex">
+                    <img src="/logo.png" alt="logo" className="flex-none h-20 -mt-3 -ml-3" />
+                    <h1 className="text-2xl font-bold flex-auto">
+                        Woodlands <br/> Computer Science
+                    </h1>
                 </a>
-                <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
-                    <ChevronRight/>Calendar
-                </a>
-                <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
-                    <ChevronRight/>Meeting Notes
-                </a>
-                <a href="/" className="flex gap-2 hp-link">
-                    <ChevronRight />
-                    <span>
-                        Hack::Peel
-                    </span>
-                </a>
+                <button className="sm:hidden hover:cursor-pointer p-3" onClick={() => setNavOpen(!navOpen)}>
+                    <ChevronDown className={"transition duration-200 " + (navOpen ? "rotate-180" : "")} />
+                </button>
+            </div>
+            <div className={"transition-[grid-template-rows] ease-in-out overflow-hidden duration-200 grid sm:block " + (navOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]")}>
+                <div className="min-h-0">
+                    <div className="flex flex-col ml-0 sm:flex-row lg:flex-col lg:ml-16 gap-3 text-nowrap flex-wrap mt-4">
+                        <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
+                            <ChevronRight/>Home
+                        </a>
+                        <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
+                            <ChevronRight/>Calendar
+                        </a>
+                        <a href="/" className="flex gap-2 transition duration-75 ease-in hover:text-brand">
+                            <ChevronRight/>Meeting Notes
+                        </a>
+                        <a href="/" className="flex gap-2 hp-link">
+                            <ChevronRight />Hack::Peel
+                        </a>
+                    </div>
+
+                </div>
             </div>
         </nav>
-        <main className="flex-auto">
+        <main className="flex-auto mt-26 sm:mt-36 lg:mt-0">
             {children}
         </main>
     </main>
