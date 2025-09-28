43c95cbc4e365dca05c7cd69af9398494f897ffc
Owen Wang <o0wang@outlook.com>
2025-09-28 18:36:25 -0400

    Push instagram url

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
 export const DISCORD_URL: string = "";
-export const INSTAGRAM_URL: string = "";
+export const INSTAGRAM_URL: string = "https://www.instagram.com/woodlands.cs/";
 export const EMAIL_URL: string = "mailto:752381@pdsb.net";
