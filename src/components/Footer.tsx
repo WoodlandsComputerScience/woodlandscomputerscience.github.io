bbaf4634f37fcda4735ec38066a90716fb5b8e71
Owen Wang <o0wang@outlook.com>
2025-09-28 21:49:48 -0400

    Label footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import {FaGithub, FaInstagram} from "react-icons/fa6";
 export default function Footer() {
     return <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row">
-            <a href={INSTAGRAM_URL} className="btn"><FaInstagram /></a>
-            <a href={GITHUB_URL} className="btn"><FaGithub /></a>
+            <a href={INSTAGRAM_URL} className="btn" aria-label="Instagram"><FaInstagram /></a>
+            <a href={GITHUB_URL} className="btn" aria-label="GitHub"><FaGithub /></a>
         </div>
         <span className="ml-4 text-right">{new Date().getFullYear()} Woodlands Computer Science</span>
     </div>
