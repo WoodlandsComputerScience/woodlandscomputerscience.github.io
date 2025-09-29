dc19f6ff16f47bf1264712591e259d11bc743181
Owen Wang <o0wang@outlook.com>
2025-09-28 22:04:57 -0400

    Remove h1 in logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
 export default function Logo() {
     return <a href="/" className="flex items-center -mt-3 no-underline">
         <img src="/logo.png" alt="logo" className="flex-none h-16 mr-3 pb-1" />
-        <h1 className="text-xl font-bold flex-auto shrink-0">
+        <span className="text-xl font-bold flex-auto shrink-0 text-white">
             Woodlands <br/> Computer Science
-        </h1>
+        </span>
     </a>
 }
