309f0ad4734d0ccd07e1bd0a3f7b385a63d83045
Owen Wang <o0wang@outlook.com>
2025-09-30 21:51:35 -0400

    Restore stickiness

--- src/components/Pane.tsx
+++ src/components/Pane.tsx
@@ -3,15 +3,15 @@ import TrafficLight from "./TrafficLight.tsx";
 export interface PaneProps {
     children?: React.ReactNode;
     trafficLight?: boolean;
-    overflow?: boolean;
+    overflowScroll?: boolean;
     className?: string;
 }
 
-export default function Pane({children, trafficLight, overflow, className} : PaneProps) {
+export default function Pane({children, trafficLight, overflowScroll, className} : PaneProps) {
     trafficLight = trafficLight ?? true;
     return <div className={"p-4 shadow-lg bg-neutral-800 rounded-3xl " + className}>
         {trafficLight && <TrafficLight />}
-        <div className={"h-full w-full " + (overflow ? ""  : "overflow-x-auto")}>
+        <div className={"h-full w-full " + (overflowScroll ? "overflow-x-auto"  : "")}>
             { children }
         </div>
     </div>
