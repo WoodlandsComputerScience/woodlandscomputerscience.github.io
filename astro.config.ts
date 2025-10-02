9abdedfa0b737947bb20dae4bd89c36cc7d7d7a3
Owen Wang <o0wang@outlook.com>
2025-10-01 22:05:05 -0400

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // @ts-expect-error
     plugins: [ tailwindcss() ]
   },
-  integrations: [ react() ],
+  integrations: [react(), sitemap()],
   markdown: {
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
   },
