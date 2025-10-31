f51f7046ef7b0d0fb98a6660269f1eb21f6283e6
Owen Wang <o0wang@outlook.com>
2025-10-30 23:44:21 -0400

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://twss.dev',
   vite: {
     // @ts-expect-error
     plugins: [ tailwindcss() ]
